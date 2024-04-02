d1ba2304ab7ad514449d92c5a6d46439172383e6
Dmytro Krapyvianskyi <dimka670020040@gmail.com>
2024-04-02 10:07:49 +0300

    calculator formation, part 2

--- js/modules/calculatorForm.js
+++ js/modules/calculatorForm.js
@@ -7,13 +7,24 @@ export const formUI = () => {
   const functionalityPart = document.createElement('div');
   functionalityPart.classList.add('functionality');
 
+  const resultBtnIndexes = [9, 19, 29, 39, 49];
+  const numberButtonIndexes = [16, 17, 18, 26, 27, 28, 36, 37, 38, 46, 48];
+
   for (let i = 0; i < 50; i++) {
     if (i === 47) {
       continue;
     }
-    const btn = document.createElement('button');
-    btn.classList.add('functionality-btn');
-    btn.setAttribute('data-type', 'type-' + i);
+
+    const btn = createButton('type-' + i);
+
+    if (resultBtnIndexes.includes(i)) {
+      btn.classList.add('result__btn');
+    }
+
+    if (numberButtonIndexes.includes(i)) {
+      btn.classList.add('number__btn');
+    }
+
     functionalityPart.appendChild(btn);
   }
 
@@ -21,4 +32,15 @@ export const formUI = () => {
   calculator.appendChild(functionalityPart);
 
   console.log(calculator);
+};
+
+const createButton = (dataType) => {
+  const btn = document.createElement('button');
+  btn.classList.add('functionality-btn');
+  btn.setAttribute('data-type', dataType);
+  // btn.addEventListener('click', () => {
+  //   console.log(btn.attributes.getNamedItem('data-type'));
+  // });
+
+  return btn;
 };
